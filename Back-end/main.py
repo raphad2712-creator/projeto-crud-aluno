3ccefa7659f3fdcc98349cab7aa92196a904dc08
Raphael David Silva Freitas <raphad2712@gmail.com>
2026-05-11 21:48:14 -0300

    Update main.py

---
+++ Back-end/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Servidor Python funcionando!"
+
+@app.route("/jaca")
+def api():
+    return {
+        "mensagem": "Minha API em Python"
+    }
+@app.route("/neymar")
+def mensagem():
+    return {
+        "mensagem": "usuario entrou!"
+    }
+   
+if __name__ == "__main__":
+    app.run(debug=True)
+
