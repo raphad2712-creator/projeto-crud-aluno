ea30cb06874d70b4c0f607cd446a33fe699f58b1
Raphael David Silva Freitas <raphad2712@gmail.com>
2026-05-11 21:42:34 -0300

    Update main.py

--- Back-end/main.py
+++ Back-end/main.py
@@ -1,2 +1,21 @@
-numero = int(input("adedawd: "))
-print(numero)
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Servidor Python funcionando!"
+
+@app.route("/jaca")
+def api():
+    return {
+        "mensagem": "Minha API em Python"
+    }
+@app.route("/neymar")
+def mensagem():
+    return {
+        "mensagem": "usuario entrou!"
+    }
+   
+if __name__ == "__main__":
+    app.run(debug=True)
